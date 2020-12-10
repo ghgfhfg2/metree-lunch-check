ba92f26819f9b730a825ffc558cd3f85590eef24
ghgfhfg2 <46801546+ghgfhfg2@users.noreply.github.com>
2020-12-10 15:55:31 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+import Header from "./components/Header";
+import Content from "./components/Content";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Content />
+    </div>
+  );
+}
---
+++ src/components/Content.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Content() {
+  return (
+    <>
+      <div className="content-box"></div>
+    </>
+  );
+}
+
+export default Content;
